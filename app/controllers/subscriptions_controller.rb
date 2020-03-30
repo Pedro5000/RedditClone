80c428b19230accbda57a78a14602b95c58891dc
Pierre <pierre.de.vreyer@gmail.com>
2020-03-30 18:44:27 +0800

    Started adding Reddit styling to the app

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,14 @@
 class SubscriptionsController < ApplicationController
   def create
     @subscription = Subscription.new(subscription_params)
-    @subscription.account_id = current_account.id
-    @subscription.save
-    redirect_to community_path(@subscription.community_id)
+    if !current_account
+      flash[:notice] = "Sign up to register to this subreddit."
+      redirect_to new_account_registration_path
+    else
+      @subscription.account_id = current_account.id
+      @subscription.save
+      redirect_to community_path(@subscription.community_id)
+    end
   end
 
   def subscription_params
