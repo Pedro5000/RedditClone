d52eb16b5ced6d382bfc5d5b9551fca4cb4703ae
Pierre <pierre.de.vreyer@gmail.com>
2020-03-30 15:35:52 +0800

    Added subscriptions and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :accounts
+  get 'u/:username' => 'public#profile', as: :profile
   resources :communities do
     resources :posts
   end
+  resources :subscriptions
+  resources :comments, only: [:create]
   root 'public#index'
 end
